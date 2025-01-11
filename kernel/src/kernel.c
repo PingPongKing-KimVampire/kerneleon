a8637ab7098d45209bbe8e42d4f82992dd5ec72f
RoWoon Lee <74223246+lru0409@users.noreply.github.com>
2025-01-11 14:00:29 +0900

    chore: 불필요한 주석 삭제

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -3,17 +3,14 @@
 #include <stdint.h>
 #include "strlen.h"
 
-/* Check if the compiler thinks you are targeting the wrong operating system. */
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
 
-/* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
 
-/* Hardware text mode color constants. */
 enum vga_color {
 	VGA_COLOR_BLACK = 0,
 	VGA_COLOR_BLUE = 1,
@@ -99,9 +96,6 @@ void terminal_writestring(const char* data)
 
 void kernel_main(void) 
 {
-	/* Initialize terminal interface */
 	terminal_initialize();
-
-	/* Newline support is left as an exercise. */
 	terminal_writestring("42");
 }
